57095fc7614145474f83a7704a642d23484834a5
ravihlb <ravihlb@protonmail.com>
2025-04-11 09:40:21 -0300

    refactor: align with course

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
+    env: {
+      browser: true,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -14,6 +17,17 @@ export default [
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+      },
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+    ],
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
     plugins: {
@@ -24,10 +38,11 @@ export default [
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unsafe-finally': 'off',
+      'react/jsx-key': 'off',
     },
   },
-]
+];
